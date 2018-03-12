47fb2241702fa9e3cffd24eba75f0a9065c46f80
Vignesh Hari <vichuhari100@gmail.com>
2018-03-13 01:20:32 +0530

    Added Info Function

--- csis18manager/delegate/views.py
+++ csis18manager/delegate/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+from .models import Delegate
 # Create your views here.
 
 def info(request):
-    pass
+    if( 'csisid' not in request.GET.keys()):
+        return JsonResponse({"statuscode" : "404"})
+    datadel = Delegate.objects.all().filter(Csisid = request.GET.get('csisid'))
+    if(len(datadel) == 0):
+        return JsonResponse({'statuscode' : "303"})
+    for i in datadel:temp = i;break
+    data = []
+    for i in Delegate.objects.get(Csisid = request.GET.get('csisid')).Tracks.all():
+        if(i.Attendence.Total_Time != 0):
+            data.append({"name" : i.TrackName , "hour" : i.Attendence.Total_Time})
+    return JsonResponse( {"name" : temp.Fname + " " + temp.Lname , "track" : data})
 
 def registermain(request):
     pass
